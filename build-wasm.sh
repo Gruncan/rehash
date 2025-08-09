5a8e42006b3ea9f40ee67e3f7e78ee25a3e504f0
Duncan Jones <duncan.da.jones@gmail.com>
2025-08-09 22:31:50 +0100

    Updated shell script to change target

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,7 +1,26 @@
-cd components
 
-cd rehash-frontend
-./build-frontend.sh
+if [ -z "$1" ]; then
+  # shellcheck disable=SC2162
+  read -p "Enter build target [debug/release]: " input
+else
+  input=$1
+fi
 
-cd ../rehash-loader
-./build-loader.sh
+
+case "$input" in
+  release )
+    target="--release"
+    ;;
+  * )
+    target="--debug"
+        ;;
+esac
+
+
+cd components || exit 1
+
+cd rehash-frontend || exit 1
+./build-frontend.sh $target
+
+cd ../rehash-loader || exit 1
+./build-loader.sh $target
--- components/rehash-loader/build-loader.sh
+++ components/rehash-loader/build-loader.sh
@@ -1,1 +1,18 @@
-wasm-pack build --target web --out-dir ../../static --dev
+if [ -z "$1" ]; then
+  # shellcheck disable=SC2162
+  read -p "Enter build target [debug/release]: " input
+else
+  input=$1
+fi
+
+
+case "$input" in
+  release )
+    target="--release"
+    ;;
+  * )
+    target="--debug"
+        ;;
+esac
+
+wasm-pack build --target web --out-dir ../../static $target
