866ec790397517cd9a022cc89247e760ca272336
Duncan Jones <duncan.da.jones@gmail.com>
2025-08-11 21:30:50 +0100

    Updated windows setup script to download and move binaries

--- scripts/setup-ffmpeg-deps-windows.ps1
+++ scripts/setup-ffmpeg-deps-windows.ps1
@@ -1,19 +1,26 @@
 cd ../
 
 $depUrl = "https://github.com/Gruncan/rehash/releases/download/ffmpeg-win64-dep/rehash-win64-dep.zip"
-
 $downloadPath = "rehash-win64-dep.zip"
-
 $depPath = "./.dependencies/ffmpeg"
+$binPath = Join-Path $depPath "rehash-win64-dep/bin/*"
+$binLoc = "rehash-desktop/codec"
 
 if (-Not (Test-Path -Path $depPath))
 {
     New-Item -ItemType Directory -Path $depPath
 }
+Clear-Host
 
 Invoke-WebRequest -Uri $depUrl -OutFile $downloadPath
 Expand-Archive -Path $downloadPath -DestinationPath $depPath -Force
 
-Write-Output "Downloaded win64 .dependencies!"
+Clear-Host
+Write-Output "Downloaded win64 dependencies!"
+
+
+Move-Item -Path $binPath -Destination $binLoc
+
+Write-Output "Moved binaries for rehash desktop build!"
 
 cd scripts
