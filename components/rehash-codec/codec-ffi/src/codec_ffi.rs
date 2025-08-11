8702f435085daaf611d553f6fa15ba2f0de5d0f4
Duncan Jones <duncan.da.jones@gmail.com>
2025-08-11 21:09:03 +0100

    Updated codec-ffi build.rs to work on windows and fixed formatting issues

--- components/rehash-codec/codec-ffi/src/codec_ffi.rs
+++ components/rehash-codec/codec-ffi/src/codec_ffi.rs
@@ -1,21 +1,26 @@
-#[cfg(target_os = "linux")]
-use libloading::Symbol;
-
+/* THIS FILE IS GENERATED DO NOT EDIT */
+use crate::RehashCodecLibrary;
 #[cfg(target_os = "windows")]
 use libloading::os::windows::Symbol;
-
-use crate::RehashCodecLibrary;
+#[cfg(target_os = "linux")]
+use libloading::Symbol;
 
 impl RehashCodecLibrary {
     pub fn print_codec_version(&self) {
         unsafe {
-            let func: Symbol<unsafe extern "C" fn()> = self.lib.get(b"print_codec_version").expect("Failed to load symbol");
+            let func: Symbol<unsafe extern "C" fn()> = self
+                .lib
+                .get(b"print_codec_version")
+                .expect("Failed to load symbol");
             func()
         }
     }
     pub fn run_codec_test(&self) {
         unsafe {
-            let func: Symbol<unsafe extern "C" fn()> = self.lib.get(b"run_codec_test").expect("Failed to load symbol");
+            let func: Symbol<unsafe extern "C" fn()> = self
+                .lib
+                .get(b"run_codec_test")
+                .expect("Failed to load symbol");
             func()
         }
     }
