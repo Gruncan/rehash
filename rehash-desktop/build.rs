26d3cfd96680be9fa1749dcddd6bf8e23f9715e6
Duncan Jones <duncan.da.jones@gmail.com>
2025-08-13 22:33:51 +0100

    Fixed build.rs for release

--- rehash-desktop/build.rs
+++ rehash-desktop/build.rs
@@ -10,23 +10,24 @@ const CODEC_NAME: &str = "rehashcodec.dll";
 fn main() {
     let profile = env::var("PROFILE").unwrap();
     let shared_object_target = format!("../target/{}/{}", profile, CODEC_NAME);
-    let shared_object_abs = fs::canonicalize(Path::new(&shared_object_target)).expect("Failed to canonicalize shared object target");
+    let shared_object_abs_res = fs::canonicalize(Path::new(&shared_object_target));
 
-    println!("cargo:rerun-if-changed={}", shared_object_abs.display());
 
-    let d = format!("codec/{}", CODEC_NAME);
-    let dest = Path::new(d.as_str());
-    if !shared_object_abs.exists() {
-        panic!("{} does not exist, ensure codec ({}) library is build", shared_object_abs.display(), CODEC_NAME);
+    if let Ok(shared_object_abs) = shared_object_abs_res {
+        println!("cargo:rerun-if-changed={}", shared_object_abs.display());
+
+        let d = format!("codec/{}", CODEC_NAME);
+        let dest = Path::new(d.as_str());
+        if !shared_object_abs.exists() {
+            panic!("{} does not exist, ensure codec ({}) library is build", shared_object_abs.display(), CODEC_NAME);
+        }
+
+        eprintln!("{}", shared_object_abs.display());
+
+        fs::copy(&shared_object_abs, dest).or_else(|e| {
+            eprintln!("Failed to copy {} to {}: {}", shared_object_abs.display(), dest.display(), e);
+            Err(e)
+        }).unwrap();
     }
-
-    eprintln!("{}", shared_object_abs.display());
-
-    fs::copy(&shared_object_abs, dest).or_else(|e| {
-        eprintln!("Failed to copy {} to {}: {}", shared_object_abs.display(), dest.display(), e);
-        Err(e)
-    }).unwrap();
-
-
     tauri_build::build()
 }
